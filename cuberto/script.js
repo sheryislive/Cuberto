c597be2e5925531eccb31b9d031cd67f12c0d6b1
asynchronousJavascriptor <harshu8545@gmail.com>
2023-08-22 20:15:55 +0530

    filler

---
+++ cuberto/script.js
@@ -0,0 +1,36 @@
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hvr", {
+  videos: ["./0.mp4", "./2.mp4", "./3.mp4"],
+});
+
+gsap.to(".fleftelm", {
+  scrollTrigger: {
+    trigger: "#fimages",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    endTrigger: ".last",
+    scrub: 1,
+  },
+  y: "-300%",
+  ease: Power1,
+});
+
+let sections = document.querySelectorAll(".fleftelm");
+Shery.imageEffect(".images", {
+  style: 4,
+  config: { onMouse: { value: 1 } },
+  slideStyle: (setScroll) => {
+    sections.forEach(function (section, index) {
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top top",
+        scrub: 1,
+        onUpdate: function (prog) {
+          setScroll(prog.progress + index);
+        },
+      });
+    });
+  },
+});
